4348b94234290ed8be3ea409f31ac023142aa325
Will Farrell <will@mojotech.com>
2014-06-18 21:49:20 -0400

    [vagrant] Refactor vagrant to use consolidated config and scripts.

--- vagrant/scripts/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-# Disable CDROM entries to prevent apt-get from prompting to insert a disk
-sed -i "/^deb cdrom:/s/^/#/" /etc/apt/sources.list
-
-date > /etc/vagrant_box_build_time
--- vagrant/scripts/zerodisk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Zero out the free space to save space in the final image
-set +e
-dd if=/dev/zero of=/EMPTY bs=1M
-set -e
-sync
-rm -vf /EMPTY
-sync
