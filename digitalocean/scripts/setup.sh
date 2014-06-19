2dc0e53eea712e56b8a63bfd2bb46b535524ea79
Will Farrell <will@mojotech.com>
2014-06-18 21:48:45 -0400

    [digitalocean] Refactor DigitalOcean template to use consolidated config and scripts.

--- digitalocean/scripts/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-
-echo 'deb http://mirrors.digitalocean.com/debian wheezy-backports main' > /etc/apt/sources.list.d/backports.list
--- digitalocean/scripts/setup_kexec.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-apt-get install -y kexec-tools
-
-mv /tmp/files/kexeced.sh /etc/init.d
-chmod 755 /etc/init.d/kexeced.sh
-ln -s ../init.d/kexeced.sh /etc/rcS.d/S00kexeced.sh
